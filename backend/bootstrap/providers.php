6dd9ccdb6f7a63bc2953299ac1f36fbead666289
Dmytro Kovalenko <dmytrokovalenko.new@gmail.com>
2025-08-07 22:34:43 -0600

    chore(docker): fix memory_limit config, install Xdebug, and add Laravel Telescope

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -1,21 +1,18 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel Core Service Providers
-    |--------------------------------------------------------------------------
-    */
+    App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Illuminate\Auth\AuthServiceProvider::class,
+    Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
     Illuminate\Broadcasting\BroadcastServiceProvider::class,
     Illuminate\Bus\BusServiceProvider::class,
     Illuminate\Cache\CacheServiceProvider::class,
-    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
     Illuminate\Cookie\CookieServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
     Illuminate\Encryption\EncryptionServiceProvider::class,
     Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
     Illuminate\Foundation\Providers\FoundationServiceProvider::class,
     Illuminate\Hashing\HashServiceProvider::class,
     Illuminate\Mail\MailServiceProvider::class,
@@ -24,16 +21,8 @@ return [
     Illuminate\Pipeline\PipelineServiceProvider::class,
     Illuminate\Queue\QueueServiceProvider::class,
     Illuminate\Redis\RedisServiceProvider::class,
-    Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
     Illuminate\Session\SessionServiceProvider::class,
     Illuminate\Translation\TranslationServiceProvider::class,
     Illuminate\Validation\ValidationServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Service Providers
-    |--------------------------------------------------------------------------
-    */
-    App\Providers\AppServiceProvider::class,
 ];
